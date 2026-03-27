74ff2c187a347105e3c8d4bdab8455052b2dd290
Zielony <niewartotupisac@gmail.com>
2026-03-28 00:35:49 +0100

    player health system

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -1,44 +1,17 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour {
+
+    #region aiminig
     private enum AimingDevice {
         Mouse, Gamepad
     }
 
+    private Vector2 prevMousePos;
     private AimingDevice currentAimingDevice = AimingDevice.Mouse;
     public GameObject Hair;
-
-    private Rigidbody2D rigidBody;
-
-    public InputAction MoveAction;
-    private Vector2 prevMousePos;
-
-    public float MovementSpeed = 3.0f;
-
-    void Start() {
-        MoveAction.Enable();
-        rigidBody = GetComponent<Rigidbody2D>();
-        prevMousePos = Mouse.current != null ? Mouse.current.position.value : Vector2.zero;
-    }
-
-    void FixedUpdate() {
-        UpdatePositionDirection();
-        UpdateHairDirection();
-    }
-
-    private void UpdatePositionDirection() {
-        Vector2 moveInput = MoveAction.ReadValue<Vector2>();
-        if (!IsZero(moveInput)) {
-            Vector2 position = rigidBody.position + moveInput * MovementSpeed * Time.fixedDeltaTime;
-            rigidBody.MovePosition(position);
-            transform.up = moveInput;
-        }
-    }
-
-    private bool IsZero(Vector2 v) {
-        return Mathf.Approximately(v.x, 0.0f) && Mathf.Approximately(v.y, 0.0f);
-    }
 
     private void UpdateHairDirection() {
         Vector2 gamepadDirection = Gamepad.current != null ? Gamepad.current.rightStick.value : Vector2.zero;
@@ -59,5 +32,65 @@ public class PlayerController : MonoBehaviour {
             var mouseWorld = Camera.main.ScreenToWorldPoint(mouseScreen);
             Hair.transform.up = transform.position - mouseWorld;
         }
+    }
+    #endregion
+
+    #region movement
+    private Rigidbody2D rigidBody;
+    public InputAction MoveAction;
+    public float MovementSpeed = 3.0f;
+
+    private void UpdatePositionDirection() {
+        Vector2 moveInput = MoveAction.ReadValue<Vector2>();
+        if (!IsZero(moveInput)) {
+            Vector2 position = rigidBody.position + moveInput * MovementSpeed * Time.fixedDeltaTime;
+            rigidBody.MovePosition(position);
+            transform.up = moveInput;
+        }
+    }
+    #endregion
+
+    #region health
+    public int MaxHealth = 3;
+    public int WoundedHealth = 1;
+    public int Health;
+
+    public bool IsAlive {
+        get {
+            return Health > 0;
+        }
+    }
+
+    public bool IsWounded {
+        get {
+            return IsAlive && Health <= WoundedHealth;
+        }
+    }
+
+    public void TakeDamage(int damage) {
+        Health = Math.Max(0, Health - Math.Max(0, damage));
+    }
+
+    public void Heal() {
+        Health = MaxHealth;
+    }
+    #endregion
+
+    void Start() {
+        MoveAction.Enable();
+        rigidBody = GetComponent<Rigidbody2D>();
+        prevMousePos = Mouse.current != null ? Mouse.current.position.value : Vector2.zero;
+        Health = MaxHealth;
+    }
+
+    void FixedUpdate() {
+        if (IsAlive) {
+            UpdatePositionDirection();
+            UpdateHairDirection();
+        }
+    }
+
+    private bool IsZero(Vector2 v) {
+        return Mathf.Approximately(v.x, 0.0f) && Mathf.Approximately(v.y, 0.0f);
     }
 }
