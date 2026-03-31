0065fa081b3f88d434136027d11ce8e681e568b9
KacperVonPrecel <144613748+KacperVonPrecel@users.noreply.github.com>
2026-03-31 16:57:20 +0200

    updated Follower class for smooth camera following with little lag

--- Assets/Scripts/Utility/Follower.cs
+++ Assets/Scripts/Utility/Follower.cs
@@ -3,9 +3,22 @@ using UnityEngine;
 public class Follower : MonoBehaviour
 {
     public Transform Target;
-    public Vector3 Offset = Vector3.zero;
+    [SerializeField] private Vector3 Offset = Vector3.zero;
+    [SerializeField] private float SmoothTime = 0.3f;
+    private Vector3 _currentVelocity = Vector3.zero;
     void Update()
     {
-        if (Target != null) transform.position = Target.transform.position+ Offset;
+        if (Target != null)
+        {
+            Vector3 targetPosition = Target.position + Offset;
+
+            transform.position = Vector3.SmoothDamp(
+                transform.position,
+                targetPosition,
+                ref _currentVelocity,
+                SmoothTime
+            );
+        }
+
     }
 }
