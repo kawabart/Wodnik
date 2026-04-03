c5a0688194c3f1afa3289a7c7a780c331376f22e
npatern <kacperkapela@gmail.com>
2026-04-03 13:30:53 +0200

    defined magic nrs as variables and added descriptions

--- Assets/Scripts/LightController.cs
+++ Assets/Scripts/LightController.cs
@@ -12,19 +12,24 @@ public class LightController : MonoBehaviour
     private LightType lightType;
     [SerializeField]
     private float range;
-    [SerializeField]
-    private float brightness;
-    [SerializeField]
-    private AnimationCurve lightCurve;
+
+    [SerializeField, Tooltip("Cuts off slightly the range of LightController compared to Light component, so that there's some visual feedback before you're considered 'lit' by the game.")]
+    private float lightRangeOverride = 0.8f;
     [SerializeField]
     private bool isOn = true;
-    [SerializeField]
+    [SerializeField, Tooltip("If true, it updates in realtime during gameplay. If false, it only reads values once on start.")]
     private bool realtime = false;
 
-    private LayerMask shadowCastingLayerMask;
-    private LayerMask playerMask;
+    //Nonbinary inputs, that only affect gameplay when using GetLightValueOnObject function. For now we only use binary IsInLight function.
+    [SerializeField] 
+    private float brightness;
+    [SerializeField, Tooltip("Affects value of light over distance.")]
+    private AnimationCurve lightCurve;
+    [SerializeField, Tooltip("Multiplyes default brightness value of Light component.")]
+    private float lightBrightnessOverride = 0.5f;
 
-
+    private LayerMask shadowCastingLayerMask; //layers that block light in gameplay
+    private LayerMask playerMask; //player's layer
 
     void Start()
     {
@@ -39,8 +44,8 @@ public class LightController : MonoBehaviour
         if (lightType == LightType.Directional)
             range = Mathf.Infinity;
         else
-            range = lightComponent.range * 0.8f;
-        brightness = lightComponent.intensity / 2;
+            range = lightComponent.range * lightRangeOverride;
+        brightness = lightComponent.intensity * lightBrightnessOverride;
 
     }
     private void Update()
@@ -89,8 +94,8 @@ public class LightController : MonoBehaviour
         float viewAngle = lightComponent.spotAngle / 2;
         Vector3 direction = target.position - transform.position + Vector3.up * .05f;
         float angle = Vector3.Angle(transform.forward, direction);
-        Color rayColor = Color.white;
 
+        Color rayColor = Color.white;
         if (angle > viewAngle)
             Debug.DrawRay(transform.position, direction * 10, Color.red);
         else
