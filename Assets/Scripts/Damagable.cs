a90abb6c8f2b758038f5f212b7ba7ce6929d9d26
npatern <107077695+npatern@users.noreply.github.com>
2026-04-12 17:09:37 +0200

    Feature/hit effects (#99)
    added new system so that objects shoot out particles on taking damage
    
    modified slightly player controller, so that player bleeds
    
    * surfaces
    
    * damage feedback

--- Assets/Scripts/Damagable.cs
+++ Assets/Scripts/Damagable.cs
@@ -3,10 +3,21 @@ using UnityEngine;
 public class Damageable : MonoBehaviour, IDamageable
 {
     public float health = 10f;
-
+    private SurfaceType surfaceType = null;
+    void Start()
+    {
+        if (GetComponent<Surface>()) surfaceType = GetComponent<Surface>().type;
+    }
     public void TakeDamage(int amount)
     {
+        if (amount < 1) return;
+
         health -= amount;
+
+        if (surfaceType!=null)
+            EffectSpawner.Instance.SpawnHit(transform.position, surfaceType);
+        else
+            EffectSpawner.Instance.SpawnHit(transform.position, Vector3.up);
 
         if (health <= 0)
             Die();
