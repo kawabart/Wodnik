3a26ceff112e8704e6252da94713f94ef49b9ba4
npatern <kacperkapela@gmail.com>
2026-04-13 19:41:06 +0200

    added events to pushables

--- Assets/Scripts/Pushable.cs
+++ Assets/Scripts/Pushable.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Pushable : MonoBehaviour, IPushable
 {
+    public UnityEvent onPush;
     private Rigidbody rb;
     void Start()
     {
@@ -11,6 +13,8 @@ public class Pushable : MonoBehaviour, IPushable
     public void Push(Vector3 force)
     {
         Debug.Log("I am pushed!");
+        if (rb.isKinematic) rb.isKinematic = false;
         rb.AddForce(force, ForceMode.Impulse);
+        onPush.Invoke();
     }
 }
