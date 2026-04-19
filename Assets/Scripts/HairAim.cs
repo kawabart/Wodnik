e8d9bb9add23b22fd45930659e6c79035ef55f98
npatern <kacperkapela@gmail.com>
2026-04-19 22:27:12 +0200

    hair tweaks

--- Assets/Scripts/HairAim.cs
+++ Assets/Scripts/HairAim.cs
@@ -12,10 +12,24 @@ public class HairAim : MonoBehaviour
     void Start()
     {
         playerController = GetComponent<PlayerController>();
+        hairGeneratorAiming.middlepoint.transform.parent = null;
+        hairGeneratorAiming.endpoint.transform.parent = null;
     }
     void Update()
     {
-        if (playerController.targetedGrabObject == null) endPoint.Target = defaultHairPosition;
-        else endPoint.Target = playerController.targetedGrabObject.transform;
+        if (playerController.targetedGrabObject == null)
+        {
+            endPoint.Target = defaultHairPosition;
+            hairGeneratorAiming.middlepointStiffness = .1f;
+            hairGeneratorAiming.dampness = .9f;
+            endPoint.SmoothTime = 0.05f;
+        }
+        else
+        {
+            endPoint.Target = playerController.targetedGrabObject.transform;
+            hairGeneratorAiming.middlepointStiffness = 0.01f;
+            hairGeneratorAiming.dampness = .7f;
+            endPoint.SmoothTime = 0.15f;
+        }
     }
 }
