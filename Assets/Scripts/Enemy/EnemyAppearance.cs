a9ac13d1f7313cb424d813d00706479540694246
npatern <kacperkapela@gmail.com>
2026-05-02 21:13:26 +0200

    enemy guards apperance wip

--- Assets/Scripts/Enemy/EnemyAppearance.cs
+++ Assets/Scripts/Enemy/EnemyAppearance.cs
@@ -6,10 +6,12 @@ public class EnemyAppearance : MonoBehaviour
     [SerializeField] private Mesh[] beards;
     [SerializeField] private Mesh[] hair;
     [SerializeField] private Mesh[] moustaches;
+    [SerializeField] private Mesh[] helmets;
     [SerializeField] private SkinnedMeshRenderer beardMeshRenderer;
     [SerializeField] private SkinnedMeshRenderer hairMeshRenderer;
     [SerializeField] private SkinnedMeshRenderer moustacheMeshRenderer;
     [SerializeField] private SkinnedMeshRenderer bodyMeshRenderer;
+    [SerializeField] private SkinnedMeshRenderer helmetMeshRenderer;
     [SerializeField] Material[] materials;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,12 +20,14 @@ public class EnemyAppearance : MonoBehaviour
         RandomizeMeshes(beardMeshRenderer, beards);
         RandomizeMeshes(hairMeshRenderer, hair);
         RandomizeMeshes(moustacheMeshRenderer, moustaches);
+        RandomizeMeshes(helmetMeshRenderer, helmets);
         Material hairMaterial = materials[Random.Range(0, materials.Length)];
         Material clothesMaterial = materials[Random.Range(0, materials.Length)]; // materials[0];//didn't like them too colorfull
         bodyMeshRenderer.sharedMaterial = clothesMaterial;
         beardMeshRenderer.sharedMaterial = hairMaterial;
         moustacheMeshRenderer.sharedMaterial = hairMaterial;
         hairMeshRenderer.sharedMaterial = hairMaterial;
+        helmetMeshRenderer.sharedMaterial = clothesMaterial;
     }
 
     private void RandomizeMeshes(SkinnedMeshRenderer meshRenderer, Mesh[] meshes)
