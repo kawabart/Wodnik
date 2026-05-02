4f0be52072c7d3504c9a952f5ec09dd005b12982
npatern <kacperkapela@gmail.com>
2026-05-02 13:39:20 +0200

    weapon models

--- Assets/Scripts/ImpactDamageDealer.cs
+++ Assets/Scripts/ImpactDamageDealer.cs
@@ -1,17 +1,29 @@
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(Rigidbody))]
 public class ImpactDamageDealer : MonoBehaviour
 {
     public float minVelocityToDamage = 3f;
+    public float minSelfVelocityToDamage = 0f;
     public float increaseVelocityNeededForPlayer = 2f;
     public int damage = 1;
     public UnityEvent onDamageDeal;
+    public Rigidbody rb;
+
+    private void Start()
+    {
+         rb = GetComponent<Rigidbody>();
+    }
     void OnCollisionEnter(Collision collision)
     {
         float impactForce = collision.relativeVelocity.magnitude;
+        float velocity = rb.linearVelocity.magnitude;
 
         if (impactForce < minVelocityToDamage)
+            return;
+
+        if (velocity < minSelfVelocityToDamage)
             return;
 
         var damageable = collision.collider.GetComponent<IDamageable>();
