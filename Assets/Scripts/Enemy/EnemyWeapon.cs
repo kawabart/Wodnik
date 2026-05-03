a2dedbb56e164289d902ebd5451d6303c1dd6c40
npatern <kacperkapela@gmail.com>
2026-05-03 11:27:47 +0200

    fixed weapon rotation on drop

--- Assets/Scripts/Enemy/EnemyWeapon.cs
+++ Assets/Scripts/Enemy/EnemyWeapon.cs
@@ -65,6 +65,10 @@ public class EnemyWeapon : MonoBehaviour
         if (weaponScriptable == null) return;
         if (weaponScriptable.onGroundPrefab == null) return;
 
-        Instantiate(weaponScriptable.onGroundPrefab, transform.position, transform.rotation);
+        Vector3 defaultRotation = weaponScriptable.onGroundPrefab.transform.eulerAngles;
+        float slotY = weaponSlot.transform.eulerAngles.y;
+        Quaternion finalRotation = Quaternion.Euler(defaultRotation.x, slotY, defaultRotation.z);
+
+        Instantiate(weaponScriptable.onGroundPrefab, weaponSlot.transform.position, finalRotation);
     }
 }
