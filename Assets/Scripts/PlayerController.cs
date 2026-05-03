ca7f997c8dc045cdc2f1b8d2cc436aa038b98178
npatern <kacperkapela@gmail.com>
2026-05-03 21:56:38 +0200

    peasant boss attack

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -351,7 +351,8 @@ public class PlayerController : MonoBehaviour, IDamageable
     public void KillTakedownTarget()
     {
         Debug.Log("Taking down -1");
-        takedownTarget.GetComponent<IDamageable>().TakeDamage(new DamageData(1));
+        if (takedownTarget!=null)
+            takedownTarget.GetComponent<IDamageable>().TakeDamage(new DamageData(1));
     }
     #endregion
 
