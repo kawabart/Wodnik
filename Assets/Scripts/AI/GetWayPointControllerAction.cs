eeecd1210a4b56b3f74ade13a14a692c6918ce1b
npatern <kacperkapela@gmail.com>
2026-05-12 15:08:56 +0200

    enemy plays animations on cave level

---
+++ Assets/Scripts/AI/GetWayPointControllerAction.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
+using Unity.Behavior;
+using Unity.Properties;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "Get Way Point Controller", story: "Interact with [component] from [object] using [animator]", category: "Action", id: "88d7707cb0d86cf68c7aca81c4d6e389")]
+public partial class GetWayPointControllerAction : Action
+{
+    [SerializeReference] public BlackboardVariable<WayPointController> Component;
+    [SerializeReference] public BlackboardVariable<GameObject> Object;
+    [SerializeReference] public BlackboardVariable<Animator> Animator;
+
+    private float timer;
+    protected override Status OnStart()
+    {
+        if (Object.Value == null) return Status.Failure;
+        if (Object.Value.TryGetComponent<WayPointController>(out WayPointController wayPointController))
+        {
+            Component.Value = wayPointController;
+            timer = wayPointController.InteractionTime;
+            Animator.Value.speed =1f;
+            Animator.Value.CrossFade(wayPointController.StateName, 0.05f);
+            return Status.Running;
+        }
+        return Status.Failure;
+    }
+
+    protected override Status OnUpdate()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0f)
+            return Status.Success;
+
+        return Status.Running;
+    }
+
+    protected override void OnEnd()
+    {
+        Animator.Value.SetTrigger("EndIdle");
+    }
+}
+
