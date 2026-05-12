50f197ea305d51ae584d0a723eb9e65b7e55bd4d
npatern <kacperkapela@gmail.com>
2026-05-12 15:08:12 +0200

    changed waypoint name for clarity

---
+++ Assets/Scripts/WayPointController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class WayPointController : MonoBehaviour
+{
+    public float InteractionTime = 5f;
+    public string StateName = "Idle";
+    public void StartInteraction(GameObject enemy)
+    {
+
+    }
+    public void StopInteraction (GameObject enemy)
+    {
+
+    }
+}
